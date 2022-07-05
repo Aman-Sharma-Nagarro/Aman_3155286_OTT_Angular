91b740dbdd5ebf066ecad339d20b47dc3950ce6c
Kartik Mehta <Kartik.Mehta@netent.com>
2022-07-05 19:51:44 +0530

    user auth complete

--- OTTAngular/src/app/user/email-based-login/email-based-login.component.ts
+++ OTTAngular/src/app/user/email-based-login/email-based-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { 
   FormBuilder,
+  FormControl,
   FormGroup,
   Validator,
   Validators
@@ -14,22 +15,81 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class EmailBasedLoginComponent implements OnInit {
 
-  // form: FormGroup;
-  // type: 'login' | 'signup' | 'reset' = 'signup';
-  // loading = false;
-  // serverMessage:  string;
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.maxLength(6), Validators.required]),
+    confirmPassword:  new FormControl('', [])
+  });
 
-  // constructor() { }
+  type: 'login' | 'signup' | 'reset' = 'signup';
+  loading = false;
+  serverMessage?:  string;
+
+  constructor(private formBuilder: FormBuilder, private angularFireAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
-      
   }
-  // ngOnInit(private formBuilder: FormBuilder, private angularFireAuth: AngularFireAuth): void {
-  //   this.form = this.formBuilder.group(
-  //     {
-  //       email:  ['', [Validators.required, Validators.email]],
-  //     }
-  //   )
-  // }
+
+  changeTypeOfForm(val: 'login' | 'signup' | 'reset') {
+    this.type = val;
+  }
+
+  get isLoginType() {
+    return this.type === 'login';
+  }
+
+  get isSignupType() {
+    return this.type === 'signup';
+  }
+
+  get isResetType() {
+    return this.type === 'reset';
+  }
+
+  get email() {
+    return this.form?.get('email');
+  }
+
+  get password() {
+    return this.form?.get('password');
+  }
+
+  get confirmPassword() {
+    return this.form?.get('confirmPassword');
+  }
+
+  get passwordDoesMatchToConfirmPassword() {
+    if (this.type !== 'signup'){
+      return true;
+    } else{
+      return this.password?.value === this.confirmPassword?.value
+    }
+  }
+
+  async onSubmit() {
+    this.loading = true;
+    const email = this.form.value.email;
+    const password = this.form.value.password;
+
+    try {
+      if (email !== null && email !== undefined && password !== null && password !== undefined) {
+        if (this.isLoginType) {
+            await this.angularFireAuth.signInWithEmailAndPassword(email, password);
+          }
+        if (this.isSignupType) {
+          await this.angularFireAuth.createUserWithEmailAndPassword(email, password);
+        }
+        if (this.isResetType) {
+          await this.angularFireAuth.sendPasswordResetEmail(email);
+          this.serverMessage = 'Check your email.'
+        }
+      }
+    } catch (err: any) {
+      this.serverMessage = err;
+    }
+
+    this.loading = false;
+
+  }
 
 }
