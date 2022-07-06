47b51a9f2a39afa83d33a5aecd7739fdbfdc73d7
Aman-Sharma-Nagarro <aman.sharma@nagarro.com>
2022-07-06 22:06:12 +0530

    added tests

--- OTTAngular/src/app/shared/shell/shell.component.spec.ts
+++ OTTAngular/src/app/shared/shell/shell.component.spec.ts
@@ -1,5 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ShellComponent } from './shell.component';
 
 describe('ShellComponent', () => {
@@ -21,3 +22,29 @@ describe('ShellComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('ShellComponent', () => {
+  let component: ShellComponent;
+  let fixture: ComponentFixture<ShellComponent>;
+  let de: DebugElement;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [ShellComponent, AngularFireAuth],
+    })
+    .compileComponents();
+  })
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShellComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    fixture.detectChanges();
+  });
+
+  it ('should create', () => {
+    expect(component).toBeTruthy();
+  })
+});
+
